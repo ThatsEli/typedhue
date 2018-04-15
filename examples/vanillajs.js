391c3825bd9b5fe9a8f6de48f217f60d7aa4822c
ThatsEli <private@thatseliyt.de>
2018-04-15 20:26:55 +0200

    added githu to package.json

--- examples/vanillajs.js
+++ examples/vanillajs.js
@@ -5,4 +5,6 @@ let bridge = new TypedHue.Bridge('192.168.2.102', (success, username) => {
 
     bridge.getLights((lights) => { console.log(lights); });
 
+    bridge.setGroupState('1', TypedHue.StateCreator.onOffState(true), () => {});
+
 }, 'SfJR8F3BZ2wb5DUToQNNFKljyNluPFIlx93KKjb4');
